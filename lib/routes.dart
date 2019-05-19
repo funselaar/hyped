c570cc6ac6b7e013cab792e6fa5fa608d8412ca7
Funs Janssen <funs.janssen@hotmail.com>
2019-05-19 16:51:25 +0200

    minor changes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'screens/home/index.dart';
-import 'theme.dart';
-import 'screens/countdown/index.dart';
 import 'screens/auth/index.dart';
 
 class Routes {
